79b0f5bf858fb97e98efca6f683799ab26f2c4c4
johnli25 <warriorjohn36@gmail.com>
2023-02-07 15:21:47 -0600

    slight changes to frontend to make json data display prettier

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,12 +10,16 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
+//import javafx.util.Pair;
+
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -24,6 +28,8 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 public class MainActivity extends AppCompatActivity {
@@ -54,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(String response) {
                         Toast.makeText(getApplicationContext(), "Response: " + response,Toast.LENGTH_SHORT).show();
                         parseJson(response);
+                        Log.d("error", "arr");
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -109,18 +116,47 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
+            Map<Integer, ArrayList<Pair<Integer, String>>> groupbyMap = new HashMap<>();
             for (int i = 0; i < json_objects.size(); i++){
-                String listIDVar, nameVar;
                 int idVar = json_objects.get(i).getInt("id");
-                listIDVar = json_objects.get(i).getString("listId");
-                nameVar = json_objects.get(i).getString("name");
-                if (nameVar.length() == 0 || nameVar == "null") {
-                    continue;
-                } else {
-                    textView.append("Name: " + nameVar + ", id: " + idVar + ", listID: " + listIDVar + "\n");
-                }
+                Integer listIDVar = json_objects.get(i).getInt("listId");
+                String nameVar = json_objects.get(i).getString("name");
+                Pair<Integer, String> pairListIdName = new Pair<>(idVar, nameVar);
+                groupbyMap.computeIfAbsent(listIDVar, k -> new ArrayList<>()).add(pairListIdName);
             }
 
+            for (Map.Entry<Integer, ArrayList<Pair<Integer, String>>> set :
+                    groupbyMap.entrySet()) {
+
+                    textView.append("listId: " + set.getKey() + ", {");
+                    boolean commaFlag = false;
+                    for (int i = 0; i < set.getValue().size(); i++){
+                        String nameVar = set.getValue().get(i).second;
+                        if (nameVar.length() == 0 || nameVar == "null") {
+                            continue;
+                        } else {
+                            if (commaFlag) {
+                                textView.append(",");
+                                commaFlag = true;
+                            }
+                            textView.append(" id: " + set.getValue().get(i).first + ", name: "
+                                    + set.getValue().get(i).second + "\n");
+                        }
+                    }
+                    textView.append(" } \n");
+            }
+
+//            for (int i = 0; i < json_objects.size(); i++){
+//                String listIDVar, nameVar;
+//                int idVar = json_objects.get(i).getInt("id");
+//                listIDVar = json_objects.get(i).getString("listId");
+//                nameVar = json_objects.get(i).getString("name");
+//                if (nameVar.length() == 0 || nameVar == "null") {
+//                    continue;
+//                } else {
+//                    textView.append("Name: " + nameVar + ", id: " + idVar + ", listID: " + listIDVar + "\n");
+//                }
+//            }
         } catch (JSONException e) {
             e.printStackTrace();
         }
